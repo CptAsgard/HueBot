8d1030616943bd51ec83e8ccf6a1c32fe0f113bd
Frank van Wattingen <frank.van.wattingen@Liquit.com>
2021-11-20 12:48:32 +0100

    Deserialize json file.

--- HueBot/Program.cs
+++ HueBot/Program.cs
@@ -1,23 +1,45 @@
-﻿namespace HueBot
+﻿using System.Text.Json;
+
+namespace HueBot
 {
+    internal class Configuration
+    {
+        public string? BridgeIp { get; set; }
+        public string? ApplicationId { get; set; }
+    }
+
     public class Program
     {
-        internal static string ApplicationId = "mgw1xThOjfP1Ks7JHah2PRiJHHD2m-0RxsoqZlNS";
+        internal static string JsonConfigurationFile = "huebot.json";
 
-        private static void Main(string[] args)
+        // command /bridgeIp=xxx.xxx.xxx.xxx
+        // command /serializeIp
+        // command /forceDiscovery
+
+        private static int Main(string[] args)
         {
+            Console.WriteLine("HueBot v0.0.0.0\ndev: Talos - GPL-2.0 License\n");
+
             // use json input for bridge ip or use argument override
+            string configurationJson = File.ReadAllText(JsonConfigurationFile);
+            Configuration? config = JsonSerializer.Deserialize<Configuration>(configurationJson);
+
+            if (config == null)
+            {
+                Console.WriteLine($"[ERROR] Could not load {JsonConfigurationFile}");
+                return -1;
+            }
 
             // if neither available or if force discover specified discover it w/ mDNS
 
-            // store bridge ip back in json
+            // store discovery bridge ip in json
 
             // request discovery button press
 
             // store application id in json
-            
-            // send test command
 
+            // send test command
+            return 0;
         }
     }
 }
