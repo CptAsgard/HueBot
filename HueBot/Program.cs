e5a6adc0dca0dc231a4d5731a4f0e75f2d871ca5
Frank van Wattingen <frank.van.wattingen@Liquit.com>
2021-11-20 11:19:16 +0100

    Update README.md with planned features.

--- HueBot/Program.cs
+++ HueBot/Program.cs
@@ -2,8 +2,21 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        internal static string ApplicationId = "mgw1xThOjfP1Ks7JHah2PRiJHHD2m-0RxsoqZlNS";
+
+        private static void Main(string[] args)
         {
+            // use json input for bridge ip or use argument override
+
+            // if neither available or if force discover specified discover it w/ mDNS
+
+            // store bridge ip back in json
+
+            // request discovery button press
+
+            // store application id in json
+            
+            // send test command
 
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HueBot
+Lightweight programmable command-line Hue controller designed for better UX.
+
+- Schedule commands for throughout the day to set states like solid color mode, disco mode,
+or turn light off/on using JSON.
+- Instantly resend commands on lights reconnecting or changing value from outside, automatically 
+reconnect to bridge, and other UX improvements.
+- Send one-off state overrides using HueBot directly or use the API to set 
+overrides when HueBot is running continuously.
+- Works on any platform and alongside embedded systems e.g. alongside DiyHue on Raspberry Pi.
